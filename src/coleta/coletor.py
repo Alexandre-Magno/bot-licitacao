d7f08346a60ef7c942c96ac902e11b6728040e67
Alexandre <elfomagnno@gmail.com>
2024-12-25 00:05:39 -0300

    feat: coletor publicacoes

--- src/coleta/coletor.py
+++ src/coleta/coletor.py
@@ -1,4 +1,9 @@
 import requests
+import tempfile
+import json
+import os
+
+import time
 
 
 class Coletor:
@@ -8,18 +13,43 @@ class Coletor:
         self.base_url = "https://pncp.gov.br/api/consulta/v1/"
         self.publicao_endpoint = "contratacoes/publicacao?"
 
-    def coleta_publicacoes(self):
+    def coleta_publicacoes(self, pagina: int, data: str, uf: str, modalidade: int):
         headers = {"accept": "*/*"}
-        params = f"dataInicial=20241220&dataFinal=20241220&codigoModalidadeContratacao=8&pagina=1"
+        params = f"dataInicial={data}&dataFinal={data}&codigoModalidadeContratacao={modalidade}&pagina={pagina}&uf={uf}"
         full_url = self.base_url + self.publicao_endpoint + params
         response = requests.get(full_url, headers=headers)
         print(full_url)
         if response.status_code == 200:
+            self.salva_json_temporario(response.json(), data, uf, pagina)
             return response
         else:
             response.raise_for_status()
 
+    def salva_json_temporario(self, data: dict, date: str, uf: str, pagina: int):
+        current_dir = os.getcwd()
+        tempo_dir = os.path.join(current_dir, "temp", date)
+        os.makedirs(tempo_dir, exist_ok=True)
+        file_path = os.path.join(tempo_dir, f"publicacoes_{uf}_{pagina}.json")
+        with open(file_path, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        print(f"JSON salvo em: {file_path}")
+        return file_path
 
-api = Coletor()
-r = api.coleta_publicacoes()
-print(r)
+    def coleta_todas_publicacoes(self, data: str, uf: str, modalidade: int):
+        pagina = 1
+        while True:
+            try:
+                print("Coletando página:", pagina)
+                response = self.coleta_publicacoes(pagina, data, uf, modalidade)
+                paginas_restantes = response.json().get("paginasRestantes", 0)
+                time.sleep(1)
+                if paginas_restantes == 0:
+                    break
+                pagina += 1
+            except requests.exceptions.HTTPError as e:
+                print(f"Erro ao coletar publicações: {e}")
+                break
+
+
+coletor = Coletor()
+response = coletor.coleta_todas_publicacoes("20241220", "go", 8)
--- tests/test_coletor.py
+++ tests/test_coletor.py
@@ -2,6 +2,10 @@ from src.coleta.coletor import Coletor
 
 
 def test_status_code():
+
     coletor = Coletor()
-    response = coletor.coleta_publicacoes()
+    response = coletor.coleta_publicacoes(1, "20241220", "go", 8)
     assert response.status_code == 200
+
+
+test_status_code()
